fa28a29acadff90eb7b7fbb9f73236745bab16e6
Andrew <abrusoe1@gmail.com>
2023-01-04 20:36:56 -0500

    Next line of story

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -3,7 +3,8 @@ public class MidTerm {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-		System.out.println("Thes monkeys are trained assassins who have worked their entire lives to be the most deadly, fruit ninjas in the world");
+		System.out.println("These monkeys are trained assassins who have worked their entire lives to be the most deadly, fruit ninjas in the world");
+		System.out.println("Unfortunately, the evil Sha-Wan and his army of vegetable ninja tigers have just discovered the hideout of Kai-Yun and his band");
 		
 	}
 
