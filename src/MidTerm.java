f52d50b2c3d8d4ac82f8b46ff8d7ce2d73cab743
cjboswell98 <116133429+cjboswell98@users.noreply.github.com>
2023-01-04 20:32:52 -0500

    Ced

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -3,7 +3,8 @@ public class MidTerm {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-
+		System.out.println("Thes monkeys are trained assassins who have worked their entire lives to be the most deadly, fruit ninjas in the world");
+		
 	}
 
 }
